124eb0998d9d9f2fc1de741a8977f814dea2d8b5
Brenda Storer <brendastorer@gmail.com>
2017-08-28 12:20:13 -0400

    Update results to include link to venue on Foursquare

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore";
 const FOURSQUARE_CLIENT_ID = "IZYP4O4X12IIUAWFET4VWM0CG1WVWS4VJTMCVU3RMVORDRGJ";
 const FOURSQUARE_CLIENT_SECRET = "TV3AXUBEPXUDCCOF5FAH0G4GOJHFGYGKERFS2UXASA5ZFQM0";
+const RESULTS_CONTAINER = $(".js-search-results");
+const SEARCH_FORM = $(".js-search-form");
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
@@ -14,26 +16,37 @@ function getDataFromApi(searchTerm, callback) {
     client_secret: FOURSQUARE_CLIENT_SECRET
   }
   const result = $.getJSON(FOURSQUARE_SEARCH_URL, query, callback);
-  console.log(result);
 }
 
-function renderResult(result) {
-  const firstPhoto = result.venue.featuredPhotos.items[0];
-  const photoUrl = `${firstPhoto.prefix}300x300${firstPhoto.suffix}`;
-  const venueName = result.venue.name;
+function getVenueResults(data) {
+  data.response.groups[0].items.forEach(function(item) {
+    const foursquareVenueUrl = `https://api.foursquare.com/v2/venues/${item.venue.id}`;
+
+    const query = {
+      m: "foursquare",
+      v: 20170823,
+      client_id: FOURSQUARE_CLIENT_ID,
+      client_secret: FOURSQUARE_CLIENT_SECRET
+    }
+    const venueInfo = $.getJSON(foursquareVenueUrl, query, displayVenueInfo);
+  });
+}
+
+function displayVenueInfo(info) {
+  RESULTS_CONTAINER.append(renderResult(info));
+}
+
+function renderResult(venueResults) {
+  const venuePath = venueResults.response.venue;
+  const venuePhoto = `${venuePath.bestPhoto.prefix}300x300${venuePath.bestPhoto.suffix}`;
+  const venueName = venuePath.name;
+  const venueLink = venuePath.canonicalUrl;
 
   return `
-    <img class="search-results__thumbnail" src="${photoUrl}" alt="${venueName}" />
+    <a href="${venueLink}" target="_blank">
+      <img class="search-results__thumbnail" src="${venuePhoto}" alt="${venueName}" />
+    </a>
   `;
-}
-
-function displayFoursquareResults(data) {
-  const resultsContainer = $(".js-search-results");
-
-  resultsContainer.empty().prop("hidden", false);
-  data.response.groups[0].items.forEach(function(item) {
-    resultsContainer.append(renderResult(item));
-  });
 }
 
 function clearInput(input) {
@@ -41,13 +54,14 @@ function clearInput(input) {
 }
 
 function watchSubmit() {
-  $(".js-search-form").submit(event => {
+  SEARCH_FORM.submit(event => {
     event.preventDefault();
     const queryTarget = $(event.currentTarget).find(".js-search-input");
     const query = queryTarget.val();
 
+    RESULTS_CONTAINER.empty().prop("hidden", false);
     clearInput(queryTarget);
-    getDataFromApi(query, displayFoursquareResults);
+    getDataFromApi(query, getVenueResults);
   });
 }
 
